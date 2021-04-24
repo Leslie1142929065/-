9456759b69d7e6f5b1d0c3a136fa5433162d3400
Leslie1142929065 <77518880+Leslie1142929065@users.noreply.github.com>
2021-04-25 00:12:34 +0800

    Update and rename 情感模型训练可视化.py to 语音情感识别结果.py

--- 语音情感识别结果.py
+++ 语音情感识别结果.py
@@ -16,7 +16,6 @@ y,sr = librosa.load(path,sr=None)
 def normalizeVoiceLen(y,normalizedLen):
     nframes=len(y)
     y = np.reshape(y,[nframes,1]).T
-    #归一化音频长度为2s,32000数据点
     if(nframes<normalizedLen):
         res=normalizedLen-nframes
         res_data=np.zeros([1,res],dtype=np.float32)
@@ -27,19 +26,17 @@ def normalizeVoiceLen(y,normalizedLen):
     return y[0]
 def getNearestLen(framelength,sr):
     framesize = framelength*sr
-    #找到与当前framesize最接近的2的正整数次方
     nfftdict = {}
     lists = [32,64,128,256,512,1024]
     for i in lists:
         nfftdict[i] = abs(framesize - i)
-    sortlist = sorted(nfftdict.items(), key=lambda x: x[1])#按与当前framesize差值升序排列
-    framesize = int(sortlist[0][0])#取最接近当前framesize的那个2的正整数次方值为新的framesize
+    sortlist = sorted(nfftdict.items(), key=lambda x: x[1])
+    framesize = int(sortlist[0][0])
     return framesize
 VOICE_LEN=32000
-#获得N_FFT的长度
 N_FFT=getNearestLen(0.25,sr)
 y, sr = librosa.load(path, sr=None)
-y = normalizeVoiceLen(y, VOICE_LEN)  # 归一化长度
+y = normalizeVoiceLen(y, VOICE_LEN)
 mfcc_data = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, n_fft=N_FFT, hop_length=int(N_FFT / 4))
 feature = np.mean(mfcc_data, axis=0)
 feature = feature.reshape((126, 1))
